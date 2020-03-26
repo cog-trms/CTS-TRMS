0337ff36cec8115ad9fd7c9e68d6aa896bc9ef2a
Dandapani, Aravindan <aravindmicromax@gmail.com>
2020-03-25 18:40:25 -0600

    added log4j2 logger

--- src/main/java/com/cognizant/trms/TalentRecruitManagementSystemApplication.java
+++ src/main/java/com/cognizant/trms/TalentRecruitManagementSystemApplication.java
@@ -3,6 +3,8 @@ package com.cognizant.trms;
 import com.cognizant.trms.model.user.*;
 import com.cognizant.trms.repository.user.*;
 import com.cognizant.trms.util.DateUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,9 +20,11 @@ import java.util.Set;
  */
 @SpringBootApplication
 public class TalentRecruitManagementSystemApplication {
+    private static final Logger log = LogManager.getLogger(TalentRecruitManagementSystemApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(TalentRecruitManagementSystemApplication.class, args);
+        log.info("Appllication started");
     }
 
     @Bean
