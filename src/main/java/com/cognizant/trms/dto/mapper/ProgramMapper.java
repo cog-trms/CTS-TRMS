07aa752f05b618e49ec3f61f3fdb7b648c885f80
Varaprasad <venkata.kotha@pearson.com>
2020-03-31 19:45:01 -0600

    Program Mapper changes

--- src/main/java/com/cognizant/trms/dto/mapper/ProgramMapper.java
+++ src/main/java/com/cognizant/trms/dto/mapper/ProgramMapper.java
@@ -10,7 +10,9 @@ import org.modelmapper.ModelMapper;
 import com.cognizant.trms.dto.model.user.AccountDto;
 import com.cognizant.trms.dto.model.user.ProgramDto;
 import com.cognizant.trms.dto.model.user.UserDto;
+import com.cognizant.trms.model.user.Account;
 import com.cognizant.trms.model.user.Program;
+import com.cognizant.trms.model.user.User;
 
 /**
  * @author Vara Kotha
@@ -20,19 +22,22 @@ public class ProgramMapper {
 
 	private static final Logger log = LogManager.getLogger(ProgramMapper.class);
 
-	public static ProgramDto toProgramDto(Program program, String all) {
+	public static ProgramDto toProgramDto(Program program) {
+		
+		Account account = program.getAccount();
+		User programMgr = program.getProgramMgr();
 
 		return new ProgramDto().setProgramName(program.getProgramName())
-				.setAccount(new ModelMapper().map(program.getAccount(), AccountDto.class))
-				.setProgramMgr(new ModelMapper().map(program.getProgramMgr(), UserDto.class));
+				.setId(program.getId())
+				.setAccount(new AccountDto()
+                .setId(account.getId())
+                .setAccountName(account.getAccountName()))
+				.setProgramManager(new UserDto()
+				.setId(programMgr.getId())
+				.setFirstName(programMgr.getFirstName())
+				.setLastName(programMgr.getLastName()));
 	}
 
-	public static ProgramDto toProgramDto(Program program) {
 
-		return new ProgramDto().setProgramName(program.getProgramName()).setProgramId(program.getId())
-				.setAccountId(program.getAccount().getId()).setAccountName(program.getAccount().getAccountName())
-				.setUserId(program.getProgramMgr().getId())
-				.setProgramMgrFullName(program.getProgramMgr().getFullName());
-	}
 
 }
